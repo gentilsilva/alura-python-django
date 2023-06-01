6d371d10f2c06f087191bda88d734f5a7178d68a
Gentil Silva <silvagentil.sg@gmail.com>
2023-06-01 19:24:24 -0300

    Adicionando datasource e configurando serializer, views e urls.py

--- 2-django-rest/1-framework/escola/serializer.py
+++ 2-django-rest/1-framework/escola/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from escola.models import Aluno, Curso, Matricula
 
 
@@ -18,3 +19,25 @@ class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model: Matricula = Matricula
         fields: str = '__all__'
+
+
+class ListaMatriculaAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(
+        source='curso.descricao')  # Atribui a descrição do curso para a variável curso e retorna a descrição na requisição -> assim não é mostrado só a pk do curso
+    periodo = serializers.SerializerMethodField()  # faz o retorno do campo periodo da classe curso por um método
+
+    class Meta:
+        model: Matricula = Matricula
+        fields: str = ['curso', 'periodo']
+
+    def get_periodo(
+            self, obj):
+        return obj.get_periodo_display()  # Retorna o periodo da mesma forma que é exibido no Django Admin
+
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+
+    class Meta:
+        model: Matricula = Matricula
+        fields: str = ['aluno_nome']
--- 2-django-rest/1-framework/escola/views.py
+++ 2-django-rest/1-framework/escola/views.py
@@ -1,21 +1,50 @@
-from rest_framework import viewsets
+from django.db.models import QuerySet
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculaAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
     queryset: Aluno = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset: Curso = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     """Exibindo todas as matriculas"""
     queryset: Matricula = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaMatriculasAluno(generics.ListAPIView):           # Esta classe é para personalizar a busca através do pk do aluno
+    """Exibindo as matriculas de um aluno(a)"""
+    def get_queryset(self):
+        queryset: QuerySet = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculaAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Exibindo alunos e alunas matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
